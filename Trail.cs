da642d3238bbf367dc0235b5cacea09e034c2f38
Martaxxxx <148713900+Martaxxxx@users.noreply.github.com>
2024-06-08 23:31:31 +0200

    Add files via upload

---
+++ Trail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TatryExplorer.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nazwa { get; set; }
+
+        [Required]
+        public double Długość { get; set; }
+
+        [Required]
+        public string PoziomTrudności { get; set; }
+
+        [Required]
+        public string OpisSzlaku { get; set; }
+
+        public string UserId { get; set; } // Dodane pole
+    }
+}
